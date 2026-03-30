20ff47a1ff1d8c64e28c50dc5ac0d0d72979587e
nikitarem <erenv97@yandex.ru>
2026-03-30 09:44:05 +0000

    миграции и команда для создания базы

--- opinions_app.py
+++ opinions_app.py
@@ -1,6 +1,8 @@
+import csv
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -52,6 +54,21 @@ def internal_error(error):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+# Команды
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открыть файл.
+    with open('opinions.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            opinion = Opinion(**row)
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}') 
 
 # Вью
 @app.route('/')

f9b0568c9981ed0e2d161f22979541de3a279b62
nikitarem <erenv97@yandex.ru>
2026-03-30 09:29:36 +0000

    обработка ошибок

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -40,12 +40,22 @@ class OpinionForm(FlaskForm):
     )
     submit = SubmitField('Добавить')
 
+# Обработчики ошибок
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 # Вью
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных мнений о фильмах нет.'
+        abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
     return render_template('index.html', opinion=opinion)
@@ -53,18 +63,20 @@ def index_view():
 @app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
     form = OpinionForm()
-
     if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash('Такое мнение уже было оставлено ранее!')
+            return render_template('add_opinion.html', form=form)
         opinion = Opinion(
-            title=form.title.data,
-            text=form.text.data,
+            title=form.title.data, 
+            text=text, 
             source=form.source.data
         )
         db.session.add(opinion)
         db.session.commit()
         return redirect(url_for('opinion_view', id=opinion.id))
-    # Если валидация не пройдена - просто отрисовать страницу с формой.
-    return render_template('add_opinion.html', form=form) 
+    return render_template('add_opinion.html', form=form)
 
 @app.route('/opinions/<int:id>')  
 def opinion_view(id):
